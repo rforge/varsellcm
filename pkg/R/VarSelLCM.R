e5d467862ec58440ad327d80bf5333c66942c663
masedki <masedki@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-01-23 13:47:27 +0000

    windows parallel
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/varsellcm@29 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/VarSelLCM.R
+++ pkg/R/VarSelLCM.R
@@ -29,7 +29,7 @@ VarSelModelSelection <- function(x, g, nbinit=30,  parallel=TRUE){
     if(Sys.info()["sysname"] == "Windows")
     {
       cl <- makeCluster(nb.cpus)
-      common.objects <- c("x","nbcl")
+      common.objects <- c("VarSelStartingPoint","x","nbcl")
       clusterExport(cl=cl, varlist = common.objects, envir = environment())
       reference <- parLapply(cl = cl, 
                              X  = nbcl, 
