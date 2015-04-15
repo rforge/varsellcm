f6cc1a50c0b7bd30c3c6e2b4939b288ccec3eb8a
marbaclo <marbaclo@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-04-15 06:52:43 +0000

    plot
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/varsellcm@75 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Plot.R
+++ pkg/R/Plot.R
@@ -1,59 +1,70 @@
+plotCateg <- function(data, model, param){
+  if (sum(model@omega)>0){
+    results <- matrix(0,model@g,data@d)
+    for (j in 1:data@d){
+      for (k in 1:model@g){
+        alpha <- 0
+        for (k2 in (c(1:model@g)[-k]))
+          alpha <- alpha + (param@pi[k2]* param@alpha[[j]][k2,])/sum(param@pi[-k])
+        
+        results[k,j] <- round(sum((param@alpha[[j]][k,]-alpha)**2),4)
+      }
+    }
+    if (any(model@omega==0))  
+      results <- results[, -which(model@omega==0)]
+    
+    mp<-barplot(results, beside = TRUE, axisnames = FALSE, names.arg=colnames(data@shortdata)[which(model@omega==1)], main="Discriminative categorical variables", ylab="Discriminative measure", ylim=c(0,max(results)), cex.main=0.8)
+    mtext(1, at = mp, text = paste("C",1:model@g), line = 0, cex = 0.5)
+    mtext(1, at = colMeans(mp), text = colnames(data@shortdata)[which(model@omega==1)], line = 2) 
+  }else
+    plot(NA, xlim=c(0,1), ylim=c(0,1),xlab = "", ylab="", axes = F, main="Discriminative categorical variables", cex.main=0.8)
+}
+
 setMethod(
   f="plot",
   signature = c("VSLCMresultsCategorical"),
   definition = function(x){
-    if (any(x@model@omega==1) && (x@model@g>1)){
-      results <- matrix(0,x@model@g,x@data@d)
-      for (j in 1:x@data@d){
-        for (k in 1:x@model@g){
-          alpha <- 0
-          for (k2 in (c(1:x@model@g)[-k]))
-            alpha <- alpha + (x@param@pi[k2]* x@param@alpha[[j]][k2,])/sum(x@param@pi[-k])
-          
-          results[k,j] <- round(mean((x@param@alpha[[j]][k,]-alpha)**2),4)
-        }
-      }
-      if (any(x@model@omega==0))  
-        results <- results[, -which(x@model@omega==0)]
-      
-      mp<-barplot(results, beside = TRUE, axisnames = FALSE, names.arg=colnames(x@data@shortdata)[which(x@model@omega==1)], main="", ylab="Discriminative measure", ylim=c(0,max(results)))
-      mtext(1, at = mp, text = paste("C",1:x@model@g), line = 0, cex = 0.5)
-      mtext(1, at = colMeans(mp), text = colnames(x@data@shortdata)[which(x@model@omega==1)], line = 2) 
-    }else{
+    if (any(x@model@omega==1) && (x@model@g>1))
+      plotCateg(x@data, x@model, x@param)
+    else
       cat("No plot is available since none variable is discriminative!")
-    }
-    
   }
 )
+
+plotCont <- function(data, model, param){
+  if (sum(model@omega)>0){
+    results <- matrix(0,model@g,data@d)
+    for (j in 1:data@d){
+      canddef <- range(data@data[,j], na.rm = T)
+      canddef <- canddef + c(-0.05,0.05)*(canddef[2] - canddef[1])
+      for (k in 1:model@g){
+        ftmp <- function(u){
+          out <- dnorm(u, param@mu[j,k], param@sd[j,k])
+          for (k2 in(c(1:model@g)[-k])) out <- out - param@pi[k2]/sum(param@pi[-k]) * dnorm(u, param@mu[j,k2], param@sd[j,k2])
+          return(out**2)
+        }
+        results[k,j] <- round(integrate(ftmp, lower = canddef[1], upper = canddef[2], subdivisions = 10**5)$value,4)
+      }
+    }
+    if (any(model@omega==0))  
+      results <- results[, -which(model@omega==0)]
+    
+    mp<-barplot(results, beside = TRUE, axisnames = FALSE, names.arg=colnames(data@data)[which(model@omega==1)], main="Discriminative continuous variables", ylab="Discriminative measure", ylim=c(0,max(results)), cex.main=0.8)
+    mtext(1, at = mp, text = paste("C",1:model@g), line = 0, cex = 0.5)
+    mtext(1, at = colMeans(mp), text = colnames(data@data)[which(model@omega==1)], line = 2)
+  }else
+    plot(NA, xlim=c(0,1), ylim=c(0,1),xlab = "", ylab="", axes = F, main="Discriminative continuous variables", cex.main=0.8)
+  
+}
 
 setMethod(
   f="plot",
   signature = c("VSLCMresultsContinuous"),
   definition = function(x){
-    if (any(x@model@omega==1) && (x@model@g>1)){
-      results <- matrix(0,x@model@g,x@data@d)
-      for (j in 1:x@data@d){
-        canddef <- range(x@data@data[,j], na.rm = T)
-        canddef <- canddef + c(-0.05,0.05)*(canddef[2] - canddef[1])
-        for (k in 1:x@model@g){
-          ftmp <- function(u){
-            out <- dnorm(u, x@param@mu[j,k], x@param@sd[j,k])
-            for (k2 in(c(1:x@model@g)[-k])) out <- out - x@param@pi[k2]/sum(x@param@pi[-k]) * dnorm(u, x@param@mu[j,k2], x@param@sd[j,k2])
-            return(out**2)
-          }
-          results[k,j] <- round(integrate(ftmp, lower = canddef[1], upper = canddef[2], subdivisions = 10**5)$value,4)
-        }
-      }
-      if (any(x@model@omega==0))  
-        results <- results[, -which(x@model@omega==0)]
-      
-      mp<-barplot(results, beside = TRUE, axisnames = FALSE, names.arg=colnames(x@data@data)[which(x@model@omega==1)], main="", ylab="Discriminative measure", ylim=c(0,max(results)))
-      mtext(1, at = mp, text = paste("C",1:x@model@g), line = 0, cex = 0.5)
-      mtext(1, at = colMeans(mp), text = colnames(x@data@data)[which(x@model@omega==1)], line = 2) 
-      
-    }else{
+    if (any(x@model@omega==1) && (x@model@g>1))
+      plotCont(x@data, x@model, x@param)    
+    else
       cat("No plot is available since none variable is discriminative!")
-    }
   }
 )
 
@@ -63,44 +74,14 @@ setMethod(
   signature = c("VSLCMresultsMixed"),
   definition = function(x){
     if (any(x@model@omega==1) && (x@model@g>1)){
-      results <- matrix(0,x@model@g,x@data@d)
-      loc <- 0
-      if (x@data@withContinuous){
-        for (j in 1:x@data@dataContinuous@d){
-          loc <- loc + 1
-          canddef <- range(x@data@dataContinuous@data[,j], na.rm = T)
-          canddef <- canddef + c(-0.05,0.05)*(canddef[2] - canddef[1])
-          for (k in 1:x@model@g){
-            ftmp <- function(u){
-              out <- dnorm(u, x@param@paramContinuous@mu[j,k], x@param@paramContinuous@sd[j,k])
-              for (k2 in(c(1:x@model@g)[-k])) out <- out - x@param@pi[k2]/sum(x@param@pi[-k]) * dnorm(u, x@param@paramContinuous@mu[j,k2], x@param@paramContinuous@sd[j,k2])
-              return(out**2)
-            }
-            results[k,loc] <- round(integrate(ftmp, lower = canddef[1], upper = canddef[2], subdivisions = 10**5)$value,4)
-          }
-        }
-      }
+      par(mfrow=c(1,x@data@withContinuous+x@data@withCategorical))
+      if (x@data@withContinuous)
+        plotCont(x@data@dataContinuous, new("VSLCMmodel", g=x@model@g, omega=x@model@omega[which(names(x@model@omega) %in% colnames(x@data@dataContinuous@data))]), x@param@paramContinuous)    
       
-      if (x@data@withCategorical){
-        for (j in 1:x@data@dataCategorical@d){
-          loc <- loc + 1
-          for (k in 1:x@model@g){
-            alpha <- 0
-            for (k2 in (c(1:x@model@g)[-k]))
-              alpha <- alpha + (x@param@pi[k2]* x@param@paramCategorical@alpha[[j]][k2,])/sum(x@param@pi[-k])
-            results[k,loc] <- round(mean((x@param@paramCategorical@alpha[[j]][k,]-alpha)**2),4)
-          }
-        }
-        
-      }
-      if (any(x@model@omega==0))  
-        results <- results[, -which(x@model@omega==0)]
+      if (x@data@withCategorical)
+        plotCateg(x@data@dataCategorical, new("VSLCMmodel", g=x@model@g, omega=x@model@omega[which(names(x@model@omega) %in% colnames(x@data@dataCategorical@shortdata))]), x@param@paramCategorical)    
       
-      mp<-barplot(results, beside = TRUE, axisnames = FALSE, names.arg= names(x@model@omega)[which(x@model@omega==1)], main="", ylab="Discriminative measure", ylim=c(0,max(results)))
-      mtext(1, at = mp, text = paste("C",1:x@model@g), line = 0, cex = 0.5)
-      mtext(1, at = colMeans(mp), text = names(x@model@omega)[which(x@model@omega==1)], line = 2) 
-    }else{
-      cat("No plot is available since none variable is discriminative!")
     }
   }
 )
+
