db8512d7d2e0b4cdb2687e034e8f0c3f99d769ba
marbaclo <marbaclo@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-03-14 10:29:01 +0000

    number
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/varsellcm@191 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/VarSelLCM.R
+++ pkg/R/VarSelLCM.R
@@ -6,7 +6,7 @@
 ##' \tabular{ll}{
 ##'   Package: \tab VarSelLCM\cr 
 ##'   Type: \tab Package\cr 
-##'   Version: \tab 2.1.0\cr
+##'   Version: \tab 2.1.1\cr
 ##'   Date: \tab 2018-03-14\cr 
 ##'   License: \tab GPL-3\cr  
 ##'   LazyLoad: \tab yes\cr
